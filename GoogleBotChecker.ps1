f5db3d6649f6259069870e831b7463ad0775e64c
GuillaumeBen <guillaume.benats@gmail.com>
2021-04-16 09:46:29 +0200

    GoogleBotChecker
    A small script to verify if a IP belongs to Google bots IPs or is potentially an unknown malicious bot.

---
+++ GoogleBotChecker.ps1
@@ -0,0 +1,20 @@
+﻿# IP to verify 
+$computerIPAddress = "66.249.72.72"
+
+# Following https://developers.google.com/search/docs/advanced/crawling/verifying-googlebot
+# Step 1: Reverse DNS lookup on target IP
+# Step 2: Forward DNS lookup on resolved hostname
+# Step 3: Verify that Reverse DNS resolves to google.com or googlebot.com AND forward DNS on resolved hostname equals target IP
+try{
+    $resolveBotOrigin = [System.Net.Dns]::GetHostEntry($computerIPAddress)[0].HostName
+    $resolvedOriginIP = (Resolve-DnsName $resolveBotOrigin)[0].IPAddress
+
+    if($resolveBotOrigin.ToLower() -like '*google.com' -And $resolvedOriginIP -eq $computerIPAddress){
+        Write-Host 'Google Bot'
+    }elseif($resolveBotOrigin.ToLower() -like '*googlebot.com' -And $resolvedOriginIP -eq $computerIPAddress){
+        Write-Host 'Google Bot 2'
+    }else{
+        Write-Host 'Malicious IP'
+    }
+
+}catch { Write-Host 'Error in the command or provided IP could not be resolved.' }
